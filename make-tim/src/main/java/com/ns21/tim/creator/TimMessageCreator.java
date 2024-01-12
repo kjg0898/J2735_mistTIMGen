9dc17b7653ca7a5596bf5e6dba1f3100b069acdc
kjg0898 <kjg0898@gmail.com>
2024-01-12 15:38:18 +0900

    2024-01-12 김진건 내용 :itis 코드 추가

--- make-tim/src/main/java/com/ns21/tim/creator/TimMessageCreator.java
+++ make-tim/src/main/java/com/ns21/tim/creator/TimMessageCreator.java
@@ -10,10 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 
 /**
  * packageName    : com.ns21.tim.creator
